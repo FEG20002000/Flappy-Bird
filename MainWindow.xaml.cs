5c2af8ae1c408517fe06d49051d1e66d4c789200
FEG20002000 <fletcherg2457@gmail.com>
2022-08-03 10:21:36 +0200

    Commit 1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+
+namespace FlappyBird
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        double score;
+        int gravity = 6;
+        bool gameOver;
+        Rect flappyBirdHitBox;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gameTimer.Tick += MainEventTimer;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            StartGame();
+        }
+
+        private void MainEventTimer(object sender, EventArgs e)
+        {
+            txtScore.Content = "Score: " + score;
+
+            flappyBirdHitBox = new Rect(Canvas.GetLeft(flappybird), Canvas.GetTop(flappybird), flappybird.Width, flappybird.Height);
+
+            Canvas.SetTop(flappybird, Canvas.GetTop(flappybird) + gravity);
+
+            if(Canvas.GetTop(flappybird) < - 10 || Canvas.GetTop(flappybird) > 458)
+            {
+                EndGame();
+            }
+
+            foreach(var x in MyCanvas.Children.OfType<Image>())
+            {
+                if((string)x.Tag == "obs1" || (string)x.Tag == "obs2" || (string)x.Tag == "obs3")
+                {
+                    Canvas.SetLeft(x, Canvas.GetLeft(x) - 5);
+
+                    if(Canvas.GetLeft(x) < -100)
+                    {
+                        Canvas.SetLeft(x, 800);
+
+                        score += 0.5;
+                    }
+
+                    Rect pipeHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+                    if(flappyBirdHitBox.IntersectsWith(pipeHitBox))
+                    {
+                        EndGame();
+                    }
+                }
+
+                if((string)x.Tag == "cloud")
+                {
+                    Canvas.SetLeft(x, Canvas.GetLeft(x) - 2);
+
+                    if(Canvas.GetLeft(x) < -250)
+                    {
+                        Canvas.SetLeft(x, 550);
+                    }
+                }
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Space)
+            {
+                flappybird.RenderTransform = new RotateTransform(-20, flappybird.Width / 2, flappybird.Height / 2);
+                gravity = -6; 
+            }
+
+            if(e.Key == Key.R && gameOver == true)
+            {
+                StartGame();
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            flappybird.RenderTransform = new RotateTransform(5, flappybird.Width / 2, flappybird.Height / 2);
+            gravity = 6;
+        }
+
+        private void StartGame()
+        {
+            MyCanvas.Focus();
+
+            int temp = 300;
+
+            score = 0;
+
+            gameOver = false;
+
+            Canvas.SetTop(flappybird, 190);
+
+            foreach(var x in MyCanvas.Children.OfType<Image>())
+            {
+                if((string)x.Tag == "obs1")
+                {
+                    Canvas.SetLeft(x, 500);
+                }
+                if ((string)x.Tag == "obs2")
+                {
+                    Canvas.SetLeft(x, 800);
+                }
+                if ((string)x.Tag == "obs3")
+                {
+                    Canvas.SetLeft(x, 1100);
+                }
+                if ((string)x.Tag == "cloud")
+                {
+                    Canvas.SetLeft(x, 300 + temp);
+                    temp = 800;
+                }
+            }
+            gameTimer.Start();
+        }
+
+        private void EndGame()
+        {
+            gameTimer.Stop();
+            gameOver = true;
+            txtScore.Content += "  Game over!! Press R to try again";
+        }
+    }
+}
